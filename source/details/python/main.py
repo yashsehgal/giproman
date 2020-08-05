bb782997d1ee46155867a272424cfc0fa77a340f
yashsehgal <yashsehgal.work@gmail.com>
2020-08-05 21:02:43 +0530

    main modules for python

---
+++ source/details/python/main.py
@@ -0,0 +1,3 @@
+from source.details.python.UserBitBucketDetails import UserBitBucketDetails as UserBitBucketDetails
+from source.details.python.UserDetails import UserDetails as UserDetails
+from source.details.python.UserGithubDetails import UserGithubDetails as UserGithubDetails
---
+++ source/git_commands/python/main.py
@@ -0,0 +1,1 @@
+from source.git_commands.python.GitCommands import GitCommands as GitCommands
